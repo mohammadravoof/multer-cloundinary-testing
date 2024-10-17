aa59a069385acea0f9cb13afbf782941a7461a52
Mohammad Ravoof <140537466+mohammadravoof@users.noreply.github.com>
2024-10-17 17:40:39 +0530

    .
    .

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -11,25 +11,28 @@ const handleUpload = (req, res) => {
     if (!req.file) {
         return res.status(400).json({ error: 'No file uploaded' });
     }
-
     // Create a readable stream from the buffer
     const bufferStream = new Readable();
     bufferStream.push(req.file.buffer);
     bufferStream.push(null); // Signal the end of the stream
-    console.log(req.file.name);
 
+    const publicId = req.file.originalname.split('.').slice(0, -1).join('.'); // Get public ID based on the original file name
+    console.log(publicId);
     // Create an upload stream for Cloudinary
-    const uploadStream = cloudinary.uploader.upload_stream((error, result) => {
-        if (error) {
-            return res
-                .status(500)
-                .json({ error: 'Upload failed', details: error });
+    const uploadStream = cloudinary.uploader.upload_stream(
+        { public_id: publicId },
+        (error, result) => {
+            if (error) {
+                return res
+                    .status(500)
+                    .json({ error: 'Upload failed', details: error });
+            }
+            res.json({
+                url: result.secure_url,
+                filename: result.public_id,
+            }); // Respond with the URL of the uploaded image
         }
-        res.json({
-            url: result.secure_url,
-            filename: result.original_filename,
-        }); // Respond with the URL of the uploaded image
-    });
+    );
 
     // Pipe the buffer stream to the upload stream
     bufferStream.pipe(uploadStream);
